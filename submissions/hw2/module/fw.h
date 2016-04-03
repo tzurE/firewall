8a9cf93947e3c0f5878c30924187ff749a59b2f1
fw <fw@FW-devel.(none)>
2016-04-03 20:20:33 +0300

    hw2 sub is ready

---
+++ submissions/hw2/module/fw.h
@@ -0,0 +1,7 @@
+
+#include <linux/module.h> /* Needed by all modules */
+#include <linux/kernel.h> /* Needed for KERN_INFO */ 
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h> 
+#include <linux/fs.h>
+#include <linux/device.h>
